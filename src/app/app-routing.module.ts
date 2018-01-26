756a01c691ab303ebeb9f2aabfa4964d61f9b0f7
Alexander Schmidt <a.schmidt@mittwald.de>
2018-01-26 22:21:10 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {UserComponent} from "./user/user/user.component";
+import {ChatComponent} from "./chat/chat/chat.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: '/user'},
+    {path: 'user', component: UserComponent},
+    {path: 'chat', component: ChatComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
