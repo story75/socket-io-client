2757f01906440292b7bf6ca3655ff29a6b29ae72
Alexander Schmidt <a.schmidt@mittwald.de>
2018-01-26 22:19:09 +0100

    Add components

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ChatComponent]
 })
 export class ChatModule { }
