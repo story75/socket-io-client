df8461ba7f3e61119634dfe634aba938d31ea21d
Alexander Schmidt <a.schmidt@mittwald.de>
2018-01-26 22:46:26 +0100

    Init socket

---
+++ src/app/shared/chat.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import * as socketIo from 'socket.io-client';
+import {environment} from '../../environments/environment';
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class ChatService {
+
+    private socket;
+
+    constructor() {
+    }
+
+
+    public connect(): void {
+        if (!this.socket) {
+            this.socket = socketIo(environment.serverUri);
+        }
+    }
+
+    public send(message: any): void {
+        this.socket.emit('message', message);
+    }
+
+    public onMessage(): Observable<any> {
+        return new Observable<any>(observer => {
+            this.socket.on('message', (data: any) => observer.next(data));
+        });
+    }
+
+    public onEvent(event: 'connect' | 'disconnect'): Observable<any> {
+        return new Observable<Event>(observer => {
+            this.socket.on(event, () => observer.next());
+        });
+    }
+}
